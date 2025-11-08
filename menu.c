05e0847b57fcb7a3be029cb3a4d48f8663f0cbce
Lukmanul Hakim Karva <lukmanul25@mhs.usk.ac.id>
2025-11-08 23:29:38 +0700

    program telah diubah oleh hakim

--- menu.c
+++ menu.c
@@ -8,3 +8,19 @@ void menu_user(char *user) {
     do {
         printf("\n=== MENU USER (%s) ===\n", user);
         printf("1. Lihat alat\n");
+        printf("2. Pinjam alat\n");
+        printf("3. Lihat pinjaman\n");
+        printf("4. Kembalikan alat\n");
+        printf("5. Logout\n");
+        printf("Pilih: ");
+        scanf("%d", &pilih);
+
+        if (pilih == 1) {
+            printf("\n%-5s %-15s %-15s %-15s %-8s %-6s\n",
+                   "ID", "Nama", "Merek", "Model", "Tahun", "Jumlah");
+            printf("-------------------------------------------------------------\n");
+            for (int i = 0; i < n; i++) {
+                printf("%-5u %-15s %-15s %-15s %-8u %-6u\n",
+                    alat[i].id, alat[i].nama, alat[i].merek,
+                    alat[i].model, alat[i].tahun, alat[i].stok);
+            }
