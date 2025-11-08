108c1ae2550c5d16e1f8da5ed579e752db029138
abdan-alt <abdans@mhs.usk.ac.id>
2025-11-08 18:05:53 +0700

    Abdan telah mengubah program

--- admin2.c
+++ admin2.c
@@ -5,4 +5,12 @@
             int idx = cari_alat(alat, n, id);
             if (idx == -1) {
                 printf("ID tidak ditemukan.\n");
+            }else {
+                printf("Nama baru: "); scanf("%s", alat[idx].nama);
+                printf("Merek baru: "); scanf("%s", alat[idx].merek);
+                printf("Model baru: "); scanf("%s", alat[idx].model);
+                printf("Tahun baru: "); scanf("%u", &alat[idx].tahun);
+                printf("Jumlah baru: "); scanf("%u", &alat[idx].stok);
+                save_tools(alat, n);
+                printf("Data alat berhasil diupdate!\n");
             }
