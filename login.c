f529e2b17e62cd35ecdafe5bd3cb961794dd4b89
abdan-alt <abdans@mhs.usk.ac.id>
2025-11-16 21:25:15 +0700

    Abdan telah mengubah program

--- login.c
+++ login.c
@@ -4,31 +4,13 @@
 #include <stdlib.h>
 #define MAX 100
 
-struct Tool {  /* Struktur untuk data alat lab */
-    unsigned int id;
-    char nama[50];
-    char merek[50];
-    char model[50];
-    unsigned int tahun;
-    unsigned int stok;
-};
-struct Akun {  /* Struktur untuk data akun */
-    char username[100];
-    char pass[100];
-    char role[10];
-};
-struct Loan {  /* Struktur untuk alat yg ingin dipinjam */
-    char username[50];
-    unsigned int id;
-    unsigned int jumlah;
-};
 void hapus_enter(char *s) {  /* fungsi untuk menghapus newline */
     int n = strlen(s);  /* menghitung panjangnya string */
     if (n > 0 && s[n-1] == '\n') {  /* jika string tidak kosong dan last char yaitu '\n' */
         s[n-1] = '\0';  /* mengganti '\n' dengan '\0' untuk mempersingkat string */
     }
 }
-int login (char *user, char *pass, char *role) {
+int login (const char *user, const char *pass, char *role) {
     FILE *f = fopen ("akun.txt", "r");
     if (!f) {  /* jika file tidak berhasil dibuka*/
         printf("File akun tidak ada\n");
@@ -36,11 +18,12 @@ int login (char *user, char *pass, char *role) {
     }
     struct Akun login;  /*mengaliaskan struct Akun menjadi login*/
     while(fscanf(f, "%s %s %s", login.username, login.pass, login.role) == 3) {  /*mengecek jika username dan pass cocok dengan inputan*/
-        if (strcmp(login.username, user) == 0 && strcmp(login.pass, pass) == 0)  /*jika kondisi awal terpenuhi, maka atur ke role*/
+        if (strcmp(login.username, user) == 0 && strcmp(login.pass, pass) == 0) {  /*jika kondisi awal terpenuhi, maka atur ke role*/
             strcpy(role, login.role);  /*menyalin peran dari login*/
             fclose(f);  /*tutup file*/
             return 1;
     }
+}
     fclose(f);
     return 0;
 }
